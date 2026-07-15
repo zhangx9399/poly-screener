fb1c46645fd0049d8fb599972ca8a559418c8f11
zhangx9399 <420751382@qq.com>
2026-07-15 14:16:48 +0800

    Remove basePath for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  basePath: "/poly",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
